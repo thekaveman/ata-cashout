d5bf085d6f248c89f376578b7b39786be660b740
Kegan Maher <kegan.maher@gmail.com>
2015-04-13 19:37:58 -0700

    providing some reusable values

--- src/js/services.js
+++ src/js/services.js
@@ -1,7 +1,21 @@
 (function () {
   "use strict";
 
-  angular
-    .module("ataCashout");
+  var app = angular.module("ataCashout");
+  
+  app
+    .value("HoursInDay", 8)
+    .value("SickCashoutRules", sickCashoutRules);
+
+  var sickCashoutRules = [
+    {
+      minYears: 0,
+      maxYears: 9,
+      usedToCashableDays: [6,6,6,5,4,3,2,1]
+    },{
+      minYears: 10,
+      maxYears: Number.MAX_VALUE,
+      usedToCashableDays: [12,12,12,11,10,9,8,7,6,5,4,3,2,1]
+   }];
 
 })();

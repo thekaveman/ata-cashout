b89c6cfd199eb07c8ced38b5dba6df238bc30204
Kegan Maher <kegan.maher@gmail.com>
2015-04-17 18:04:00 -0700

    reorg and use directives instead of ngInclude

--- src/js/directives.js
+++ src/js/directives.js
@@ -2,6 +2,39 @@
   "use strict";
 
   angular
-    .module("ataCashout");
+    .module("ataCashout")
+    .directive("accrualsPanel", accrualsPanel)
+    .directive("jobPanel", jobPanel)
+    .directive("resultsPanel", resultsPanel);
+
+  function accrualsPanel() {
+    return {
+      restrict: "E",
+      scope: {
+        member: "="
+      },
+      templateUrl: "partials/accruals-panel.html"
+    };
+  }
+
+  function jobPanel() {
+    return {
+      restrict: "E",
+      scope: {
+        member: "="
+      },
+      templateUrl: "partials/job-panel.html"
+    };
+  }
+
+  function resultsPanel() {
+    return {
+      restrict: "E",
+      scope: {
+        result: "="
+      },
+      templateUrl: "partials/results-panel.html"
+    };
+  }
 
 })();

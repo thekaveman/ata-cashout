71f1a6c0c95cddbe397a0b5a5259c179a3e46e4a
Kegan Maher <kegan.maher@gmail.com>
2015-04-24 21:48:35 -0700

    broken tests

--- tests/calculator/CalculatorController.js
+++ tests/calculator/CalculatorController.js
@@ -10,33 +10,33 @@ describe("CalculatorController", function() {
     $controller("CalculatorController", { $scope: scope });
   }));
 
-  it("should initialize with a member with empty accrued", function() {
-    expect(scope.member).toEqual({ accrued: {} });
+  it("should initialize with a current member with empty accrued", function() {
+    expect(scope.member.current).toEqual({ accrued: {} });
   });
 
-  it("should initialize with an empty result that is not ready", function() {
-    expect(scope.result).toEqual({ ready: false });
+  it("should initialize with a member result that is not ready", function() {
+    expect(scope.member.result).toEqual({ ready: false });
   });
 
   describe("calc.go", function() {
-    it("should make the result ready", function() {
-      expect(scope.result.ready).toBe(false);
+    it("should make the member result ready", function() {
+      expect(scope.member.result.ready).toBe(false);
       scope.calc.go();
-      expect(scope.result.ready).toBe(true);
+      expect(scope.member.result.ready).toBe(true);
     });
   });
 
   describe("calc.reset", function() {
-    it("should reset to a member with empty accrued", function() {
-      scope.member = { prop1: "something", prop2: ["zero","one"] };
+    it("should reset to a current member with empty accrued", function() {
+      scope.member.current = { prop1: "something", prop2: ["zero","one"] };
       scope.calc.reset();
-      expect(scope.member).toEqual({ accrued: {} });
+      expect(scope.member.current).toEqual({ accrued: {} });
     });
 
-    it("should reset to an empty result that is not ready", function() {
-      scope.member = { prop1: "something", prop2: ["zero","one"] };
+    it("should reset to a member result that is not ready", function() {
+      scope.member.result = { ready: true, something: ["one", "two"] }
       scope.calc.reset();
-      expect(scope.result).toEqual({ ready: false });
+      expect(scope.member.result).toEqual({ ready: false });
     });
   });
 });

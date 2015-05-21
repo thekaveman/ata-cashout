0fe89ac9a901ace118fd15008717fa89e57ce290
Kegan Maher <kegan.maher@gmail.com>
2015-05-20 23:07:48 -0700

    updating vacation

--- tests/vacation/services.js
+++ tests/vacation/services.js
@@ -9,9 +9,7 @@ describe("VacationCashout", function() {
 
   beforeEach(inject(function(VacationCashout) {
     vacationCashout = VacationCashout;
-    member = {
-      accrued: { vacation: 5 }
-    };
+    member = {};
   }));
 
   it("should initialize members on evaluation", inject(function(Members) {
@@ -21,13 +19,54 @@ describe("VacationCashout", function() {
   }));
 
   it("should return member's accrued on evaluation", function() {
+    member.accrued = { vacation: 10 }
     var result = vacationCashout.evaluate(member);
-    expect(result.accrued).toBe(5);
+    expect(result.accrued).toBe(10);
   });
 
   it("should evaluate in whole increments of hoursInDay", function() {
+    member.accrued = { vacation: 5 };
     var result = vacationCashout.evaluate(member);
     expect(result.cashable).toBe(4);
     expect(result.diff).toBe(1);
+  });
+
+  describe("member with less than 10 years", function() {
+    beforeEach(function() {
+      member.serviceYears = 8;
+      member.accrued = { vacation: 42 };
+    });
+
+    it("should be able to cash up to 40 hours", function() {
+      var result = vacationCashout.evaluate(member);
+      expect(result.cashable).toBe(40)
+      expect(result.diff).toBe(2);
+    });
+  });
+
+  describe("member with more than 10 years and less than 15 years", function() {
+    beforeEach(function() {
+      member.serviceYears = 12;
+      member.accrued = { vacation: 62 };
+    });
+
+    it("should be able to cash up to 40 hours", function() {
+      var result = vacationCashout.evaluate(member);
+      expect(result.cashable).toBe(60)
+      expect(result.diff).toBe(2);
+    });
+  });
+
+  describe("member with 15 or more years", function() {
+    beforeEach(function() {
+      member.serviceYears = 20;
+      member.accrued = { vacation: 82 };
+    });
+
+    it("should be able to cash up to 80 hours", function() {
+      var result = vacationCashout.evaluate(member);
+      expect(result.cashable).toBe(80)
+      expect(result.diff).toBe(2);
+    });
   });
 });

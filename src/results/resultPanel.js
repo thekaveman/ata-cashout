56de47d09b95e5065be6b2737e4e2249259feb09
Kegan Maher <kegan.maher@gmail.com>
2015-05-21 23:36:55 -0700

    rename, consolidate config values

---
+++ src/results/resultPanel.js
@@ -0,0 +1,31 @@
+(function () {
+  "use strict";
+
+  angular
+    .module("ataCashout.results")
+      .directive("resultPanel", resultPanel)
+      .factory("resultPanelConfig", resultPanelConfig);
+
+  function resultPanel() {
+    return {
+      restrict: "E",
+      scope: {
+        config: "=",
+      },
+      templateUrl: "results/resultPanel.html"
+    };
+  }
+
+  function resultPanelConfig() {
+    return {
+      new: function(config) {
+        return {
+          heading: config.heading || "",
+          member: config.member || {},
+          nonCashableRule: config.nonCashableRule || {},
+          result: config.result || {}
+        };
+      }
+    };
+  }
+})();

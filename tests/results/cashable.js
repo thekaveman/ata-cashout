267a465669a882304ee3050acc05cdd36bb3f09c
Kegan Maher <kegan.maher@gmail.com>
2015-05-25 16:26:10 -0700

    cleanup

--- tests/results/cashable.js
+++ tests/results/cashable.js
@@ -1,45 +1,38 @@
 "use strict";
-describe("cashable", function() {
+describe("cashableController", function() {
   beforeEach(module("ataCashout.results"));
 
-  describe("cashableController", function() {
-    var scope;
+  var scope;
 
-    beforeEach(inject(function($rootScope, $controller) {
-      scope = $rootScope.$new();
-      $controller("cashableController", { $scope: scope });
-    }));
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    $controller("cashableController", { $scope: scope });
+  }));
 
-    it("should return result.cashable as cashableHours", function() {
-      scope.result = { cashable: 5 };
-      expect(scope.cashableHours()).toBe(5);
-    });
+  it("should calculate cashableAmount from cashableHours and member's payRate", function() {
+    scope.cashableHours = function() {
+      return 5;
+    };
+    scope.result = {
+      member: {
+        payRate: 2.00
+      }
+    };
 
-    it("should calculate cashableAmount from cashableHours and member's payRate", function() {
-      scope.cashableHours = function() {
-        return 5;
-      };
-      scope.result = {
-        member: {
-          payRate: 2.00
-        }
-      };
+    expect(scope.cashableAmount()).toBe("10.00");
+  });
 
-      expect(scope.cashableAmount()).toBe('10.00');
-    });
+  it("should show only when cashableHours > 0", function() {
+    scope.cashableHours = function() {
+      return 5;
+    };
 
-    it("should show only when cashableHours > 0", function() {
-      scope.cashableHours = function() {
-        return 5;
-      };
+    expect(scope.show()).toBe(true);
 
-      expect(scope.show()).toBe(true);
+    scope.cashableHours = function() {
+      return 0;
+    };
 
-      scope.cashableHours = function() {
-        return 0;
-      };
-
-      expect(scope.show()).toBe(false);
-    });
+    expect(scope.show()).toBe(false);
   });
 });
